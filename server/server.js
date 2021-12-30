354ff37f2ebd04bd97c88769f479d79d03c94837
joseph-aswath <josephaswath@gmail.com>
2021-12-30 15:20:49 +0530

    server.js pool created

--- server/server.js
+++ server/server.js
@@ -3,10 +3,18 @@ const fs = require('fs');
 const bodyParser = require('body-parser');
 const app = express();
 const router = express.Router();
+const Pool = require('pg').Pool;
+const pool = new Pool ({
+    user:'me',
+    password:'password',
+    database:'api',
+    host:'localhost',
+    port:5432
+})
 /*********************************************************************/
 app.use(router);
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.urlencoded({extended:true}));
 /*********************************************************************/
 
 
