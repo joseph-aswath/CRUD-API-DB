e81e28c86ad780d4dc6a8b27fb09a8f9e6b92958
joseph-aswath <josephaswath@gmail.com>
2021-12-31 13:00:12 +0530

    did stuff

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const app = express();
 const router = express.Router();
 const Pool = require('pg').Pool;
+/******************************************************************* */
 const pool = new Pool ({
     user:'me',
     password:'password',
@@ -16,5 +17,7 @@ app.use(router);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 /*********************************************************************/
-
+const PORT = process.env.port || 8000;
+console.log(`server port : ${PORT}`);
+/******************************************************************* */
 
